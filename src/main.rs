31e754e4eb42c4f0d3350a70d82dbe14b8389184
Juraj Paluba <juraj.palubajp@gmail.com>
2023-12-09 16:49:11 +0100

    main: remove test code

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,13 @@ mod connection;
 mod cli_args;
 mod operations;
 
-use std::io::{Write, self};
-use std::env;
 use clap::Parser;
 
 use connection::Connection;
 use cli_args::{CliParameters, CLIOperation};
 use operations::{register, login, list_plants, create_plant};
 
-fn main() -> Result<(), Box<reqwest::Error>> {
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
+fn main() -> () {
     let connection = Connection::build_connection();
 
     let args = CliParameters::parse();
@@ -22,21 +18,4 @@ fn main() -> Result<(), Box<reqwest::Error>> {
         CLIOperation::Register(register_args) => register(register_args, connection),
         CLIOperation::CreatePlant(create_plant_args) => create_plant(create_plant_args, connection),
     }
-
-    // let token = match env::var("VEGETATION_TOKEN") {
-    //     Ok(value) => value,
-    //     Err(_) => "".to_string(),
-    // };
-    // writeln!(handle, "token {:?}", token).unwrap();
-
-
-    // let response = connection.send_login()?;  // handle error with match
-    // let status_code = response.status();
-    // writeln!(handle, "status code: {:?}", status_code).unwrap();
-    // let body: LoginResponse = match response.json() {
-    //     Ok(body) => serde_json::from_value(body).unwrap(),
-    //     Err(e) => panic!("Error while getting body of login response: {:?}", e)
-    // };
-    // writeln!(handle, "body: {:?}", body).unwrap();
-    Ok(())
 }
